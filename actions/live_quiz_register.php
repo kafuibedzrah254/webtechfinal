3656e673fa9fb281d8d3c7ced68237ded00a3815
kafuibedzrah254 <kafuibedzrah254@gmail.com>
2024-05-21 09:18:58 +0000

    new push

--- actions/live_quiz_register.php
+++ actions/live_quiz_register.php
@@ -10,13 +10,13 @@ $features= $_POST['features'];
 // Insert user data into database
 $sql = "INSERT INTO `product` (pname)  
         VALUES ('$name')";
-//$sql_one= "INSERT INTO `product_feature` (feature_name)  
+$sql_one= "INSERT INTO `product_feature` (feature_name)  
 //VALUES ('$features')";
 $result = mysqli_query($con, $sql);
 
-//$result_one = mysqli_query($con,$sql_one);
+$result_one = mysqli_query($con,$sql_one);
 
-if($result){
+if($result && $result_one){
     echo '<script>
     alert ("Registration successful");
     window.location = "live_quiz_registration.php";
